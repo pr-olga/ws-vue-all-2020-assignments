1e8e3683e33ec1e2b07e0e69f7f0263586177b97
pr-olga <frkhorkovaolga@gmail.com>
2020-11-12 20:10:18 +0100

    feat: add ex-6

---
+++ ex-6/src/App.vue
@@ -0,0 +1,85 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link>
+  </div>
+  <router-view/>
+  <!--
+   TODO
+   Assignment 8:
+   Extract Comment into seprate component
+  -->
+  <!--
+    For the sake of simplicity, example contains the code
+    that is nessessary for this assignment only.
+  -->
+</template>
+
+<style lang="scss">
+/**
+ * For the sake of simplicity, example contains the code
+ * that is nessessary for this assignment only.
+ */
+ @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap');
+
+ /* General
+    ========================================================================== */
+
+ html,
+ body {
+   height: 100%;
+   padding: 0;
+   margin: 0;
+ }
+
+ *,
+ *::before,
+ *::after {
+   box-sizing: border-box;
+   margin: 0;
+   padding: 0;
+ }
+
+ .button {
+   width: 80px;
+   height: 40px;
+   border-radius: 3px;
+   background-color: #fff;
+ }
+
+ .button:hover {
+   cursor: pointer;
+   outline-color: #292929;
+ }
+
+ .button--red {
+   border: 2px solid #e21912;
+   outline-color: #e21912;
+   color: #e21912;
+ }
+
+ .button--green {
+   border: 2px solid #008f39;
+   outline-color: #008f39;
+   color: #008f39;
+ }
+
+ .button--black {
+   border: 2px solid #292929;
+   outline-color: #292929;
+   color: #292929;
+ }
+
+ #app {
+   padding: 2rem;
+   background-color: #eef0f1;
+   color: #292929;
+   font-size: 16px;
+   font-family: Poppins, sans-serif;
+ }
+
+ .block {
+   max-width: 750px;
+   margin: 0 auto 20px auto;
+   background-color: #fff;
+ }
+</style>
---
+++ ex-6/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <article-single/>
+    <discussion/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ArticleSingle from '@/components/ArticleSingle.vue'
+import Discussion from '@/components/Discussion.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    'article-single': ArticleSingle,
+    'discussion': Discussion
+  }
+}
+</script>
